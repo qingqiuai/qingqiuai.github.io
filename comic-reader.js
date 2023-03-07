a0c459e914929860bf3902294f50ba3abeba108f
qingqiuai <113910218+qingqiuai@users.noreply.github.com>
2023-03-07 14:14:42 +0800

    Update comic-reader.js

--- comic-reader.js
+++ comic-reader.js
@@ -11,13 +11,13 @@ if (!comicUrl) {
 loadComic(comicUrl);
 
 // 加载漫画文件
-function loadComic(url) {
+// function loadComic(url) {
     // 通过JavaScript创建一个<iframe>标签，并将漫画文件的URL作为src属性值
-    var iframe = document.createElement('iframe');
-    iframe.src = url;
+    // var iframe = document.createElement('iframe');
+    // iframe.src = url;
     // 将<iframe>标签添加到DOM树中，使其可以显示漫画
-    document.body.appendChild(iframe);
-}
+    // document.body.appendChild(iframe);
+// }
 
 // 处理错误
 window.onerror = function(message, source, lineno, colno, error) {
